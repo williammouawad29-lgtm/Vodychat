c52b64d27a9d7a008d02535034221b26c8694995
williammouawad29-lgtm <williammouawad29@gmail.com>
2026-02-09 14:41:12 +0100

    Add server.js with Express and Socket.io setup

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hello World</h1>');
+});
+
+io.on('connection', (socket) => {
+    console.log('A user connected');
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
